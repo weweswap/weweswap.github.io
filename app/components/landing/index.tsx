4d04d6fa79e17f9418c6922791326c2712415bf0
ehsan <ehsan.saradar@gmail.com>
2024-07-17 16:15:45 +0330

    Fix header

--- app/components/landing/index.tsx
+++ app/components/landing/index.tsx
@@ -2,11 +2,41 @@
 
 export default function Sections() {
     return (
-        <section className="header-custom h-[100vh]">
-            <div className="max-w-screen-xl mx-auto">
-            section landing
+        <>
+            <section className="header-custom h-[100vh]">
+                <div className="max-w-screen-xl mx-auto">
+                    <div className="w-full h-[450px] flex justify-center items-center flex-col">
+                        <h1 className="text-8xl">
+                            WEWE LOVE <br /> THE YIELDS
+                        </h1>
+                        <p className="text-4xl mt-2">Together We Earn</p>
+                    </div>
 
-            </div>
-        </section>
+
+                </div>
+            </section>
+            <section className="main-content min-h-[100vh]">
+                <div className="flex">
+                    <div className="w-[50%]">
+                        <div className="lg:w-[70%] w-full ml-auto">
+                            <h4 className="text-3xl">WAMMv1</h4>
+                            <p>
+                                {`WAMMv1 revolutionizes how we perceive LP Positions. 
+                                Instead of viewing your assets as fixed and exposed to impermanent loss, we treat them as dynamic, working assets that generate yield. 
+                                Just like real-world businesses, these assets can either appreciate or depreciate in value. 
+                                The key difference with WEWESWAP is that your yield is always received in USD stablecoins, providing you with the stability and assurance of "cash" returns (awesome!).`}
+
+                            </p>
+                        </div>
+                    </div>
+                    <div className="w-[50%]">
+                        50
+
+                    </div>
+                </div>
+            </section>
+        </>
+
+
     )
 }
