8a74be326afb44ca734278ddabe64fd335ef804a
ehsan-saradar <ehsan.saradar@gmail.com>
2024-07-24 14:12:23 +0330

    add scroll to top

---
+++ app/assets/icons/ChevronUp.tsx
@@ -0,0 +1,9 @@
+export function IconChevronUp() {
+    return (
+        <svg className="inline-block h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={4} stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+        </svg>
+    )
+
+
+}
--- app/shared-componets/footer.tsx
+++ app/shared-componets/footer.tsx
@@ -3,9 +3,34 @@ import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { footerCopy } from "@/app/copy/footer";
+import { useEffect, useState } from "react";
+import { IconChevronUp } from "../assets/icons/ChevronUp";
 
 export default function Footer() {
     const pathName = usePathname()
+    const [isVisible, setIsVisible] = useState(false);
+    const isBrowser = () => typeof window !== 'undefined';
+
+
+    const handleScroll = () => {
+        if (window.scrollY > 700) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
+    function scrollToTop() {
+        if (!isBrowser()) return;
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
     return (
         <footer>
             {pathName != "/" &&
@@ -22,7 +47,13 @@ export default function Footer() {
                 ))}
 
             </div>
-
+            {/* scroll to top */}
+            <button
+                className={`lg:opacity-0 fixed bottom-0 right-0 bg-turquoise text-black rounded-full p-4 mr-6 mb-[30px] z-50 items-center text-xs flex gap-2 scrollToTopButton ${isVisible ? 'visible' : ''}`}
+                onClick={scrollToTop}
+            >
+                <IconChevronUp />
+            </button>
         </footer>
 
     );
