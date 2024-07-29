d6e88b8126cb5ab26cabae28940e4e2e1a37b4f0
ehsan <ehsan.saradar@gmail.com>
2024-07-29 22:35:34 +0330

    Fix production

--- next.config.mjs
+++ next.config.mjs
@@ -1,30 +1,4 @@
-// next.config.mjs
-import { TerserPlugin } from 'terser-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true, // Optional: Enables additional React checks in development
-  swcMinify: true, // Enables the use of SWC for minification
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.optimization.minimizer = [
-        new TerserPlugin({
-          terserOptions: {
-            compress: {
-              drop_console: true, // Remove console logs in production
-            },
-            output: {
-              comments: false, // Remove comments in production
-            },
-          },
-        }),
-      ];
-    }
-    return config;
-  },
-
-  productionBrowserSourceMaps: false, // Disable source maps in production
-};
+const nextConfig = {};
 
 export default nextConfig;
