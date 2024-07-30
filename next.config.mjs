5bc274cedc003a1f790a8f1b08b14c0dbcf62f8d
ehsan-saradar <ehsan.saradar@gmail.com>
2024-07-30 12:43:48 +0330

    terser plugin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+import TerserPlugin from 'terser-webpack-plugin';
+const nextConfig = {
+    swcMinify: true,
+    webpack: (config, { dev, isServer }) => {
+        if (!dev && !isServer) {
+            config.optimization.minimize = true;
+            config.optimization.splitChunks = {
+                chunks: 'all',
+                minSize: 20000,
+                maxSize: 100000,
+            };
+            config.optimization.minimizer = [
+                new TerserPlugin({
+                    terserOptions: {
+                        format: {
+                            comments: false,
+                        },
+                        compress: true,
+                        mangle: true,
+                    },
+                    extractComments: false,
+                }),
+            ];
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
