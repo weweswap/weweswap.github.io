8e794effc1d34ca5f32ef0646c77b6f74a306486
mohammad reza ebrhimi <mr.ebrahimi96@gmail.com>
2024-09-30 14:51:58 +0200

    fix: remove header links

--- app/copy/header.ts
+++ app/copy/header.ts
@@ -1,24 +1,24 @@
 export const headerCopy = {
-    navbarLinks: [
-        {
-            name: "POOLS",
-            url: "",
-            target: ""
-        },
-        {
-            name: "EARNINGS",
-            url: "",
-            target: ""
-        },
-        {
-            name: "CHAOS",
-            url: "",
-            target: ""
-        }
-    ],
-    button: {
-        name: "LAUNCH",
-        url: "https://app.weweswap.com/merge",
-        target: "_blank"
-    }
-}
+  navbarLinks: [
+    // {
+    //     name: "POOLS",
+    //     url: "",
+    //     target: ""
+    // },
+    // {
+    //     name: "EARNINGS",
+    //     url: "",
+    //     target: ""
+    // },
+    // {
+    //     name: "CHAOS",
+    //     url: "",
+    //     target: ""
+    // }
+  ],
+  button: {
+    name: "LAUNCH",
+    url: "https://app.weweswap.com/merge",
+    target: "_blank",
+  },
+};
